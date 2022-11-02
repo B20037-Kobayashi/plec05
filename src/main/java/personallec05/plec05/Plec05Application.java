ee4e8f2529791c9fcb3968a9c87782e4524b0059
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-11-02 10:35:38 +0900

    初期化

---
+++ src/main/java/personallec05/plec05/Plec05Application.java
@@ -0,0 +1,13 @@
+package personallec05.plec05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plec05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plec05Application.class, args);
+	}
+
+}
---
+++ src/test/java/personallec05/plec05/Plec05ApplicationTests.java
@@ -0,0 +1,13 @@
+package personallec05.plec05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plec05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
