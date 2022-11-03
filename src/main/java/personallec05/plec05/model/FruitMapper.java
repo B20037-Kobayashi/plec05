340362a78366582a3829a3d41e05a94498ee6574
B20037-Kobayashi <e1b20037@oit.ac.jp>
2022-11-03 18:07:14 +0900

    sample5-3

--- src/main/java/personallec05/plec05/model/FruitMapper.java
+++ src/main/java/personallec05/plec05/model/FruitMapper.java
@@ -4,10 +4,17 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Delete;
 
 @Mapper
 public interface FruitMapper {
 
   @Select("SELECT ID, NAME,PRICE from FRUIT")
   ArrayList<Fruit> selectAllFruit();
+
+  @Select("SELECT ID, NAME, PRICE from FRUIT WHERE ID = #{id}")
+  Fruit selectById(int id);
+
+  @Delete("DELETE FROM FRUIT WHERE ID = #{id}")
+  boolean deleteById(int id);
 }
